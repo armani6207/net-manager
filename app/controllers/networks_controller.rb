512cda3e67d0e292008186302bc2348ab515e2c7
Armando <rojas.armani@gmail.com>
2021-05-10 18:04:32 -0400

    Added DevicesController

--- app/controllers/networks_controller.rb
+++ app/controllers/networks_controller.rb
@@ -4,9 +4,7 @@ class NetworksController < ApplicationController
     end
 
     def create
-        find_network
-
-        @network.new(network_params)
+        @network = Network.new(network_params)
         if @network.save
             redirect_to network_path(@network)
         else
@@ -19,10 +17,14 @@ class NetworksController < ApplicationController
         find_network
     end
 
+    def edit
+        find_network
+    end
+
     private
 
     def network_params
-        require(:network).permit(:name)
+        params.require(:network).permit(:name)
     end
 
     def find_network
