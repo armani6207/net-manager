b970720208beb6cd400a403ec970a837d076ad45
Armando <rojas.armani@gmail.com>
2021-05-10 18:06:30 -0400

    Added device routes and methods

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,3 +1,27 @@
 class DevicesController < ApplicationController
 
+    def new
+
+    end
+
+    def create
+
+    end
+
+    def show
+
+    end
+
+    def index
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
 end
