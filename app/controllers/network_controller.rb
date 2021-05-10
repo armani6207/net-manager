f1a480215e2cbd5f971947159c43c51550a88530
Armando <rojas.armani@gmail.com>
2021-05-10 16:23:40 -0400

    added routes for network controller

--- app/controllers/network_controller.rb
+++ app/controllers/network_controller.rb
@@ -1,3 +1,9 @@
 class Network_controller < ApplicationCOntroller
+    def new
+        @network = Network.new
+    end
 
+    def create
+
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   get '/' => 'static#home'
 
+  resources :networks
+
 end
