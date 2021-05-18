4b28c228dbfce60ddf9f344d7a126ffd0f06510e
Armando <rojas.armani@gmail.com>
2021-05-18 08:43:43 -0400

    Added NavBar and css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def logged_in?(session)
+        !!session[:user_id]
+    end
+
+    def current_user(session)
+        @user = User.find(session[:user_id])
+    end
+
 end
